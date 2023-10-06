9567e2c8da92271e1be3f91e39383dadc3151379
Aai'sha Fisher <aaishafisher484@gmail.com>
2023-10-06 13:40:00 +0200

    challenge 2 complete

---
+++ Challenge 2/script.js
@@ -0,0 +1,27 @@
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+if (nickname) {                                       //checks if it falsy or truthy
+ console.log(`Good Morning, ${nickname}!`)
+} else if (firstname){
+    console.log(`Good morning, ${firstname}!`)
+} else {
+    console.log('Good morning!')
+}
+
+
+
+
+
+
+
+
+
+
+// ALGORITHM
+// collect firstname
+// collect nickname
+// greet the consumer with data they input:
+// if nickname is truthy, display that
+// if not, display firstname
+// if both are falsey, display greeting with no name
